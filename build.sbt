001ddd918a353aca0719822faa56a6f4e16e7843
sara hatami <sara.hatami@snapptrip.com>
2020-01-15 17:04:39 +0330

    add DiagonalDifference

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "algorithms"
 version := "0.1"
 
 scalaVersion := "2.13.1"
+
+libraryDependencies++=Seq(
+  "org.scalatest" %% "scalatest" % "3.0.8" % "test"
+)
--- src/main/scala/MainRunner.scala
+++ src/main/scala/MainRunner.scala
@@ -1,5 +1,25 @@
 object MainRunner {
   def main(args: Array[String]): Unit = {
-    println("first try")
+    val arr = Array(Array(1,2,3),Array(4,5,6),Array(10,20,30))
+    println(DiagonalDifference(arr))
   }
+
+  def DiagonalDifference(matrix: Array[Array[Int]]) = {
+
+    var j=0;
+    var i=matrix.size-1
+    var sum1 , sum2=0
+
+    matrix.foreach{ ar =>
+      sum1 += ar(j)
+      j+=1
+    }
+    matrix.foreach{ ar =>
+      sum2+=ar(i)
+      i-=1
+    }
+
+    scala.math.abs(sum2 - sum1)
+  }
+
 }
