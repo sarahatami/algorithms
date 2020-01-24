a42a893d8b87ef8e905d33f0b81b045c6a90553b
sara hatami <sara.hatami@snapptrip.com>
2020-01-24 21:23:58 +0330

    created Plusminus class

--- src/main/scala/MainRunner.scala
+++ src/main/scala/MainRunner.scala
@@ -1,13 +1,13 @@
 object MainRunner {
   def main(args: Array[String]): Unit = {
 //    val arr = Array(Array(1, 2, 3), Array(4, 5, 6), Array(10, 20, 30))
-    val arr2 = Array(1,2,3,0,-1,-2)
-    println(plusminus(arr2))
+//    val enter= Array(1,1,1,0,0,0,0,0,-1,-1)
+//    val x = Plusminus(enter)
+//    x.plusMinus
 //    staircase(7)
   }
 
   def DiagonalDifference(matrix: Array[Array[Int]]) = {
-
     var j = 0;
     var i = matrix.size - 1
     var sum1, sum2 = 0
@@ -20,13 +20,13 @@ object MainRunner {
       sum2 += ar(i)
       i -= 1
     }
-
     scala.math.abs(sum2 - sum1)
   }
-  case class plusminus(arr:Array[Int]) {
 
-    def plusMinus(arr: Array[Int]): (Int, Int, Int) = {
 
+  case class Plusminus(Arr:Array[Int]) {
+    var arr= Arr
+    def plusMinus = {
       var positive, negative, zero: Int = 0
       arr.foreach { a =>
         a match {
@@ -35,12 +35,12 @@ object MainRunner {
           case 0 => zero += 1
         }
       }
-      (positive, negative, zero)
+      println(positive, negative, zero)
     }
   }
 
-  def staircase(n: Int) {
 
+  def staircase(n: Int) {
     for(i <- 1 to n ){
       for(j <- 1 to n-i){
         print(" ")
@@ -50,7 +50,6 @@ object MainRunner {
       }
       print("\n")
     }
-
   }
 
 }
